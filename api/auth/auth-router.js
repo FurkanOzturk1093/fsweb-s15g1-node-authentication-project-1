2d0c36031f993a20566b6bbda5f3e20cb20598be
Alita1093 <furkanozturk1635@gmail.com>
2023-03-06 23:35:12 +0300

    s15-g1

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,9 @@
 // `checkUsernameFree`, `checkUsernameExists` ve `checkPasswordLength` gereklidir (require)
 // `auth-middleware.js` deki middleware fonksiyonları. Bunlara burda ihtiyacınız var!
 
+const mw = require("../auth/auth-middleware");
+const router = require("express").Router();
+const userModel = require("../users/users-model");
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,6 +28,19 @@
   }
  */
 
+router.post(
+  "/register",
+  mw.sifreGecerlimi,
+  mw.usernameBostami,
+  async (req, res, next) => {
+    try {
+      let insertedUser = await userModel.ekle(req.body);
+      res.status(201).json(insertedUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -42,6 +58,14 @@
   }
  */
 
+router.post("/login", mw.usernameVarmi, async (req, res, next) => {
+  try {
+    req.session.user_id = req.user.user_id;
+    res.status(200).json({ message: `Hoşgeldin ${req.user.username}!` });
+  } catch (error) {
+    next(error);
+  }
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +82,29 @@
     "message": "Oturum bulunamadı!"
   }
  */
+router.get("/logout", async (req, res, next) => {
+  try {
+    if (req.session.user_id) {
+      req.session.destroy((err) => {
+        if (err) {
+          next({ message: "Logout methodunda hata oluştu" });
+        } else {
+          next({
+            status: 200,
+            message: "Çıkış yapildi",
+          });
+        }
+      });
+    } else {
+      next({
+        status: 200,
+        message: "Oturum bulunamadı!",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
- 
 // Diğer modüllerde kullanılabilmesi için routerı "exports" nesnesine eklemeyi unutmayın.
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -21,11 +21,41 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+const session = require("express-session");
+const SessionStore = require("connect-session-knex")(session);
+
+const authRouter = require("../api/auth/auth-router");
+const userRouter = require("../api/users/users-router");
+
+server.use(
+  session({
+    name: "cikolatacips",
+    secret: "secret_cikolatacips",
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+    },
+    store: new SessionStore({
+      knex: require("../data/db-config"),
+      tableName: "sessions",
+      sidFieldName: "sid",
+      createTable: true,
+      clearInterval: 1000 * 60 * 60,
+    }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+server.use("/api/users", userRouter);
+server.use("/api/auth", authRouter);
+
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
